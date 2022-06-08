422f2fd3ee365591c59998694c814635217c0281
maximeDzN <maxime.dazin@gmail.com>
2022-06-08 12:16:58 +0200

    save

--- src/test/java/io/codoctet/ikea/service/FoodServiceTest.java
+++ src/test/java/io/codoctet/ikea/service/FoodServiceTest.java
@@ -11,14 +11,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class FoodServiceTest {
@@ -44,7 +40,59 @@ class FoodServiceTest {
 
         MatcherAssert.assertThat(foodSet.size(), Matchers.equalTo(intendedSize));
         verify(foodRepository).findAll();
-
     }
+
+    @Test
+    @DisplayName("food_getOne_returnOneFromDB")
+    void food_getOne_returnOneFromDB(){
+
+        Long expectedId = 1L;
+        Optional<Food> expectedFood = Optional.ofNullable(Food.builder().id(expectedId).name("Boulette suédoise").price(15).build());
+
+        when(foodRepository.findById(expectedId)).thenReturn(expectedFood);
+        Food food = foodService.get(1L);
+
+        MatcherAssert.assertThat(food, Matchers.hasToString("Food(id=1, price=15.0, name=Boulette suédoise)"));
+        verify(foodRepository).findById(expectedId);
+    }
+
+    @Test
+    @DisplayName("food_create_insertInDB")
+    void food_create_insertInDB(){
+
+        Food inputFood = Food.builder().name("Boulette suédoise").price(15).build();
+        Food expectedFood = Food.builder().id(1L).name("Boulette suédoise").price(15).build();
+
+        when(foodRepository.save(inputFood)).thenReturn(expectedFood);
+        Food food = foodService.create(inputFood);
+
+        MatcherAssert.assertThat(food.getId(), Matchers.equalTo(expectedFood.getId()));
+        verify(foodRepository).save(inputFood);
+    }
+
+    @Test
+    @DisplayName("food_update_insertInDB")
+    void food_update_insertInDB(){
+
+
+        Food expectedFood = Food.builder().id(1L).name("Boulette marocaine").price(39).build();
+
+        when(foodRepository.save(expectedFood)).thenReturn(expectedFood);
+        Food food = foodService.update(expectedFood);
+
+        MatcherAssert.assertThat(food.getId(), Matchers.equalTo(expectedFood.getId()));
+        verify(foodRepository).save(expectedFood);
+    }
+
+    @Test
+    @DisplayName("food_delete_deleteFromDB")
+    void food_delete_deleteFromDB(){
+
+        Long deleteId = 1L;
+        doNothing().when(foodRepository).deleteById(deleteId);
+        foodService.delete(deleteId);
+        verify(foodRepository).deleteById(deleteId);
+    }
+
 
 }
