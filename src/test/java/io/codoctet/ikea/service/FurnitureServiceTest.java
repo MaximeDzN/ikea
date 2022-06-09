dca2941db7961cdc2920711a2e476e78da3e92f0
Flavien <monstro28500@gmail.com>
2022-06-09 09:27:36 +0200

    JenkinsFile and suite

--- src/test/java/io/codoctet/ikea/service/FurnitureServiceTest.java
+++ src/test/java/io/codoctet/ikea/service/FurnitureServiceTest.java
@@ -7,6 +7,7 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runners.Suite;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
